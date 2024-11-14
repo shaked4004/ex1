f02c113f1e9877328e94aa6338e34bfc59030d75
shaked <shaked4004@gmail.com>
2024-11-14 23:12:55 +0200

    Finished second exercise

--- ex1.c
+++ ex1.c
@@ -13,15 +13,15 @@ int main() {
   printf("What bit:\n");
   /*Scan two integers (representing number and a position)
   Print the bit in this position. */
-  int n,p:
+  int n,p;
   scanf("%d", &n);
   scanf("%d", &p);
 
   n = n >> p;
-  n = n & 1
-  
-  printf("The number is: %d", &n);
-  
+  n = n & 1;
+
+  printf("The number is: %d", n);
+
   // Set bit
   printf("\nSet bit:\n");
   /*Scan two integers (representing number and a position)
@@ -29,18 +29,37 @@ int main() {
   Print the output
   Now make sure it's "off" (equal to 0)
   Print the output */
+  scanf("%d", &n);
+  scanf("%d", &p);
+
+  int mask = 1 << p;
+  n = n | mask;
+  printf("The number is: %d", n);
+
+
+
+
+
+  scanf("%d", &n);
+  scanf("%d", &p);
+
+  int mask = 1 << p;
+  mask = ~ mask ;
+  n = n & mask;
+  printf("The number is: %d", n);
+
 
   // Toggle bit
   printf("\nToggle bit:\n");
   /*Scan two integers (representing number and a position)
   Toggle the bit in this position
   Print the new number */
-  
+
   // Even - Odd
   printf("\nEven - Odd:\n");
   /* Scan an integer
   If the number is even - print 1, else - print 0. */
-  
+
   // 3, 5, 7, 11
   printf("\n3, 5, 7, 11:\n");
   /* Scan two integers in octal base
@@ -48,6 +67,6 @@ int main() {
   Print only 4 bits, in positions: 3,5,7,11 in the result. */
 
   printf("Bye!\n");
-  
+
   return 0;
 }
